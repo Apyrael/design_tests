cf78282079bc276f067ba4917c73aa1bf49c0a58
Richard Kuiper <apyrael@gmail.com>
2016-05-25 11:27:49 +0200

    Finished the design for the styled toolbar

--- app/src/main/java/com/apyrael/designtests/StyledToolbarActivity.java
+++ app/src/main/java/com/apyrael/designtests/StyledToolbarActivity.java
@@ -2,7 +2,12 @@ package com.apyrael.designtests;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Activity that will demonstrate a styled toolbar.
@@ -15,5 +20,34 @@ public class StyledToolbarActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_styled_toolbar);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tlbStyledToolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.styled_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.mnuItemShare:
+                Toast.makeText(StyledToolbarActivity.this, "You clicked on the share button.", Toast.LENGTH_SHORT).show();
+                return true;
+            case android.R.id.home:
+                super.onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
