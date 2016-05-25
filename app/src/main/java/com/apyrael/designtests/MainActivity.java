3b1d905b340e52af734fc833fd92b052bd0c6068
Richard Kuiper <apyrael@gmail.com>
2016-05-25 10:53:32 +0200

    Added the StyledToolbarActivity

--- app/src/main/java/com/apyrael/designtests/MainActivity.java
+++ app/src/main/java/com/apyrael/designtests/MainActivity.java
@@ -1,13 +1,31 @@
 package com.apyrael.designtests;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.btnStyledToolbar) Button btnStyledToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnStyledToolbar)
+    public void onEncryptClick() {
+        Intent startActivityIntent = new Intent(MainActivity.this, StyledToolbarActivity.class);
+        startActivity(startActivityIntent);
     }
 }
---
+++ app/src/main/java/com/apyrael/designtests/StyledToolbarActivity.java
@@ -0,0 +1,19 @@
+package com.apyrael.designtests;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Activity that will demonstrate a styled toolbar.
+ *
+ * Created by richard on 2016/05/25.
+ */
+public class StyledToolbarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_styled_toolbar);
+    }
+}
